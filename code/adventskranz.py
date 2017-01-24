e63fc18a030cd3956b7fd3b4c235609ed74ffa3d
pediehl <pediehl@googlemail.com>
2017-01-24 15:38:29 +0100

    Revert "Merged RPi.GPIO into master"
    This reverts commit df2c77199f262d3283e39f2ef5af86f8a1433df8.

--- code/adventskranz.py
+++ code/adventskranz.py
@@ -1,19 +1,10 @@
-#!/usr/bin/env python
-
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+from gpiozero import LED
 import datetime
 
-led_erster_advent = 6
-led_zweiter_advent = 13
-led_dritter_advent = 19
-led_vierter_advent = 26
-
-GPIO.setup(led_erster_advent, GPIO.OUT)
-GPIO.setup(led_zweiter_advent, GPIO.OUT)
-GPIO.setup(led_dritter_advent, GPIO.OUT)
-GPIO.setup(led_vierter_advent, GPIO.OUT)
+led_erster_advent = LED(6)
+led_zweiter_advent = LED(13)
+led_dritter_advent = LED(19)
+led_vierter_advent = LED(26)
 
 #aktuelle Zeit ermitteln
 now = datetime.datetime.now()
@@ -31,13 +22,13 @@ erster_advent = weihnachten - datetime.timedelta(wochentag + 22)
 #LEDS aktivieren je nach Datum / Advent-Sonntag
 if(now <= weihnachten):
     if(now >= erster_advent):
-        GPIO.output(led_erster_advent, GPIO.HIGH)
+        led_erster_advent.on()
     if(now >= zweiter_advent):
-        GPIO.output(led_zweiter_advent, GPIO.HIGH)
+        led_zweiter_advent.on()
     if(now >= dritter_advent):
-        GPIO.output(led_dritter_advent, GPIO.HIGH)
+        led_dritter_advent.on()
     if(now >= vierter_advent):
-        GPIO.output(led_vierter_advent, GPIO.HIGH)
+        led_vierter_advent.on()
 
 #Ausgabe der Uebersicht auf dem Bildschirm
 print now.strftime('Die Advent-Sonntage im Jahr %Y') 
